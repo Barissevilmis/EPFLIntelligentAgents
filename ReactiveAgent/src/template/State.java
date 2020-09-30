0f8c00ac2b7985eaf3518c692f2d9ee4c17e9142
barissevilmis <baris.sevilmis@epfl.ch>
2020-09-30 19:03:35 +0200

    Fixed

--- ReactiveAgent/src/template/State.java
+++ ReactiveAgent/src/template/State.java
@@ -1,15 +1,49 @@
 package template;
 
-import logist.topology.Topology.City;
+public class State implements Comparable<State> {
+	public int cityId;
+	public int taskCityId;
+	
+	public State(int cityId, int taskCityId) {
+		this.cityId = cityId;
+		this.taskCityId = taskCityId;
+	}
 
-public class State 
-{
-	public final City from;
-	public final City to;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + cityId;
+		result = prime * result + taskCityId;
+		return result;
+	}
 
-	public State(City from, City to) 
-	{
-		this.from = from;
-		this.to = to;
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		State other = (State) obj;
+		if (cityId != other.cityId)
+			return false;
+		if (taskCityId != other.taskCityId)
+			return false;
+		return true;
+	}
+	
+	@Override
+	public String toString() {
+		return "(" + cityId + "," + taskCityId + ")";
+	}
+	
+	@Override
+	public int compareTo(State other) {
+		if (cityId == other.cityId)
+			return taskCityId - other.taskCityId;
+		else
+			return cityId - other.cityId;
 	}
 }
